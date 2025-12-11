bdb545804879d91916170c99439288c95542860a
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-11 15:23:36 +0100

    Dichiarazioni varibili di input

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+//PREPARAZIONE
+
+//andiamo a dichiarare tutti i campi input
+const nameUser = document.getElementById("nameUser");
+
+const ageUser = document.getElementById("ageUser");
+
+const KmUser = document.getElementById("KmUser");
+
+const buttonUser = document.getElementById("buttonUser");
+
