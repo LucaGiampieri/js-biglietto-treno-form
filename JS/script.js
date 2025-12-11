30939e433e6aa9ce87400157e2125800b7374529
Luca Giampieri <luca.giampieri95@gmail.com>
2025-12-11 15:40:54 +0100

    DIchiarazione varibili di output

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,22 @@
 //PREPARAZIONE
 
 //andiamo a dichiarare tutti i campi input
+const nameUserField = document.getElementById("nameUserField");
+
+const ageUserField = document.getElementById("ageUserField");
+
+const KmUserField = document.getElementById("KmUserField");
+
+const buttonUser = document.getElementById("buttonUser");
+
+//andiamo a dichiarare le varibili di oputput
+
 const nameUser = document.getElementById("nameUser");
 
 const ageUser = document.getElementById("ageUser");
 
-const KmUser = document.getElementById("KmUser");
+const KmUser = document.getElementById("kmUser");
 
-const buttonUser = document.getElementById("buttonUser");
+const priceUser = document.getElementById("priceUser");
+
 
